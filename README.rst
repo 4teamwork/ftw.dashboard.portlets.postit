3c07fdf2f6088dc34eb990a44998a53f5d27dc4d
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2011-12-02 12:57:50 +0100

    prepared for github

---
+++ README.rst
@@ -0,0 +1,32 @@
+
+========
+
+The postit portlet let you add small notes on your dashbord.
+
+
+Install
+======
+
+- Add ``ftw.dashboard.portlets.postit`` to your buildout configuration
+
+:: 
+  [instance]
+  eggs = 
+    ftw.dashboard.portlets.postit
+    
+- Run buildout
+
+- Install ``ftw.dashboard.portlets.postit`` in portal_setup
+
+
+Links
+=====
+
+- Package repository: https://github.com/4teamwork/ftw.dashboard.portlets.postit
+- Issue tracker: https://github.com/4teamwork/ftw.dashboard.portlets.postit/issues
+- Package on pypi: http://pypi.python.org/pypi/ftw.dashboard.portlets.postit
+
+Maintainer
+==========
+
+This package is produced and maintained by `4teamwork GmbH <http://www.4teamwork.ch/>`_.
--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 from setuptools import setup, find_packages
 import os
 
-version = open("ftw/dashboard/portlets/postit/version.txt").read().strip()
+version = '1.4dev'
 
 setup(name='ftw.dashboard.portlets.postit',
       version=version,
-      description="portlets in the ftw.dashboard",
-      long_description=open("README.txt").read() + "\n" +
+      description="Postit portlet for the dashboard",
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='',
-      author='philippe gross',
+      maintainer='philippe gross',
+      author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
-      url='http://plone.org/products/ftw.dashboard.portlets.postit/',
-      license='GPL',
+      url='https://github.com/4teamwork/ftw.dashboard.portlets.postit',
+      license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw', 'ftw.dashboard', 'ftw.dashboard.portlets'],
       include_package_data=True,
@@ -28,5 +32,7 @@ setup(name='ftw.dashboard.portlets.postit',
       ],
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
